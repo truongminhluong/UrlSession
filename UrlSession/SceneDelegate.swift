cbd1f13b17a21292a2aa6d17e2eda6472b254e96
truongminhluong <truongminhluong2003@gmail.com>
2023-10-25 17:26:18 +0700

    url

--- UrlSession/SceneDelegate.swift
+++ UrlSession/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let windowScreen = (scene as? UIWindowScene) else { return }
+        
+        let mainVC = ViewController()
+        
+        window?.windowScene = windowScreen
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- UrlSession/ViewController.swift
+++ UrlSession/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let session = URLSession.shared
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        let task = session.dataTask(with: url) { data, response, error in
+            if error != nil || data == nil {
+                print("Client error!")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                print("Server error!")
+                return
+            }
+            
+            guard let mime = response.mimeType, mime == "application/json" else {
+                print("Wrong MIME type!")
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print(json)
+            } catch {
+                print("JSON error: \(error.localizedDescription)")
+            }
+            
+            
+        }
+        task.resume()
+        
     }
-
-
+    
 }
 
